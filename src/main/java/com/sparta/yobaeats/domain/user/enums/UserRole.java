a0319dce779c97bdd92d89aa185496f983ae5701
goo3oo <goo3oo0918@gmail.com>
2025-01-08 12:06:28 +0900

    feat: User 엔티티 기본 세팅                               - 이넘 추가 (UserRole)                                  - 예외 추가 (InvalidUserRoleException)                  - 이넘 변경 (ErrorCode)

---
+++ src/main/java/com/sparta/yobaeats/domain/user/enums/UserRole.java
@@ -0,0 +1,18 @@
+package com.sparta.yobaeats.domain.user.enums;
+
+import com.sparta.yobaeats.global.exception.InvalidUserRoleException;
+import com.sparta.yobaeats.global.exception.error.ErrorCode;
+import java.util.Arrays;
+import org.springframework.http.HttpStatus;
+
+public enum UserRole {
+    USER,
+    OWNER;
+
+    public static UserRole of(String role) {
+        return Arrays.stream(UserRole.values())
+            .filter(userRole -> userRole.name().equalsIgnoreCase(role))
+            .findFirst()
+            .orElseThrow(()-> new InvalidUserRoleException(ErrorCode.INVALID_USER_ROLE));
+    }
+}
