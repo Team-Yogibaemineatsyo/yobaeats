5254c0b920e2bb9aa7e88e657e702c2553dc5236
goo3oo <goo3oo0918@gmail.com>
2025-01-08 23:01:26 +0900

    refactor: service 계층의 @Transactional 메서드 수준으로 변경

--- src/main/java/com/sparta/yobaeats/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/yobaeats/domain/review/service/ReviewService.java
@@ -1,21 +1,14 @@
 package com.sparta.yobaeats.domain.review.service;
 
-import com.sparta.yobaeats.domain.order.entity.Order;
-import com.sparta.yobaeats.domain.order.entity.Order.Status;
 import com.sparta.yobaeats.domain.order.service.OrderService;
 import com.sparta.yobaeats.domain.review.dto.request.ReviewReq;
 import com.sparta.yobaeats.domain.review.dto.response.ReviewRes;
 import com.sparta.yobaeats.domain.review.entity.Review;
-import com.sparta.yobaeats.domain.review.exception.DuplicateReviewException;
-import com.sparta.yobaeats.domain.review.exception.InvalidOrderStatusException;
 import com.sparta.yobaeats.domain.review.exception.InvalidStarRangeException;
 import com.sparta.yobaeats.domain.review.repository.ReviewRepository;
-import com.sparta.yobaeats.domain.store.entity.Store;
 import com.sparta.yobaeats.domain.store.service.StoreService;
-import com.sparta.yobaeats.domain.user.entity.User;
 import com.sparta.yobaeats.domain.user.service.UserService;
 import com.sparta.yobaeats.global.exception.NotFoundException;
-import com.sparta.yobaeats.global.exception.UnauthorizedException;
 import com.sparta.yobaeats.global.exception.error.ErrorCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
@@ -32,6 +25,7 @@ public class ReviewService {
     private final StoreService storeService;
     private final OrderService orderService;
 
+    @Transactional
     public void createReview(ReviewReq req) {
         //토큰에서 userId 받아와서 User 객체 생성
        /* User user = userService.findUserById(tokenId);
@@ -51,10 +45,9 @@ public class ReviewService {
         }
 
         Review review = req.to(user, order, store);
-        reviewRepository.save(review);*/
+        */
     }
 
-    @Transactional(readOnly = true)
     public List<ReviewRes> findByStoreId(Long storeId) {
         List<Review> reviewList = reviewRepository
             .findAllByStoreIdOrderByUpdatedAtDesc(storeId);
@@ -64,7 +57,6 @@ public class ReviewService {
             .toList();
     }
 
-    @Transactional(readOnly = true)
     public List<ReviewRes> findByStar(int startStar, int endStar) {
         if (startStar > endStar) {
             throw new InvalidStarRangeException(ErrorCode.INVALID_STAR_RANGE);
@@ -79,6 +71,7 @@ public class ReviewService {
             .toList();
     }
 
+    @Transactional
     public void deleteReview(Long reviewId) {
         Review review = reviewRepository.findById(reviewId)
             .orElseThrow(() -> new NotFoundException(ErrorCode.REVIEW_NOT_FOUND));
