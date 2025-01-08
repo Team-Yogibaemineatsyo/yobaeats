afc2ce82f41aba954c23f39f25424089e4268e03
goo3oo <goo3oo0918@gmail.com>
2025-01-08 17:07:40 +0900

    fix: User 패키지에 누락된 임포트 추가

--- src/main/java/com/sparta/yobaeats/domain/user/entity/User.java
+++ src/main/java/com/sparta/yobaeats/domain/user/entity/User.java
@@ -1,7 +1,11 @@
 package com.sparta.yobaeats.domain.user.entity;
 
 import com.sparta.yobaeats.domain.common.BaseEntity;
+import com.sparta.yobaeats.domain.user.dto.UserRes;
+import com.sparta.yobaeats.domain.user.dto.UserUpdateReq;
 import com.sparta.yobaeats.domain.user.enums.UserRole;
+import com.sparta.yobaeats.domain.user.exception.UserDeletedException;
+import com.sparta.yobaeats.global.exception.error.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
--- src/main/java/com/sparta/yobaeats/domain/user/service/UserService.java
+++ src/main/java/com/sparta/yobaeats/domain/user/service/UserService.java
@@ -1,8 +1,16 @@
 package com.sparta.yobaeats.domain.user.service;
 
+import com.sparta.yobaeats.domain.user.dto.UserDeleteReq;
+import com.sparta.yobaeats.domain.user.dto.UserRes;
+import com.sparta.yobaeats.domain.user.dto.UserUpdateReq;
+import com.sparta.yobaeats.domain.user.entity.User;
 import com.sparta.yobaeats.domain.user.repository.UserRepository;
+import com.sparta.yobaeats.global.exception.NotFoundException;
+import com.sparta.yobaeats.global.exception.UnauthorizedException;
+import com.sparta.yobaeats.global.exception.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
