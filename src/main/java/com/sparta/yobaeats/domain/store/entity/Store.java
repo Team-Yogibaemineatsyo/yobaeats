3aca2f4c40f3c4461fc7a4dfd665eda7a08c95c0
SeuneKeun <eiziong1@gmail.com>
2025-01-08 12:01:06 +0900

    feat: 가게 엔티티 구현

--- src/main/java/com/sparta/yobaeats/domain/store/entity/Store.java
+++ src/main/java/com/sparta/yobaeats/domain/store/entity/Store.java
@@ -1,15 +1,23 @@
 package com.sparta.yobaeats.domain.store.entity;
 
+import com.sparta.yobaeats.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "stores")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 public class Store {
 
     @Id
@@ -17,8 +25,49 @@ public class Store {
     @Column(name = "store_id")
     private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer minOrderPrice;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Double starRate;
+
+    @Column(nullable = false)
+    private LocalTime openAt;
+
+    @Column(nullable = false)
+    private LocalTime closeAt;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isDeleted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
     @Builder
-    public Store(Long id) {
+    public Store(
+            Long id,
+            String name,
+            Integer minOrderPrice,
+            Double starRate,
+            LocalTime openAt,
+            LocalTime closeAt,
+            boolean isDeleted,
+            User user
+    ) {
         this.id = id;
+        this.name = name;
+        this.minOrderPrice = minOrderPrice;
+        this.starRate = starRate;
+        this.openAt = openAt;
+        this.closeAt = closeAt;
+        this.isDeleted = isDeleted;
+        this.user = user;
     }
 }
