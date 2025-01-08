308598488db834e9e9923f2a7d6026f30a50879f
goo3oo <goo3oo0918@gmail.com>
2025-01-08 17:11:31 +0900

    refactor: User Entity Builder에 Long id 매개변수 추가

--- src/main/java/com/sparta/yobaeats/domain/user/entity/User.java
+++ src/main/java/com/sparta/yobaeats/domain/user/entity/User.java
@@ -40,7 +40,8 @@ public class User extends BaseEntity {
     private boolean isDeleted = false;
 
     @Builder
-    public User(String email, String password, String nickName, UserRole role) {
+    public User(Long id, String email, String password, String nickName, UserRole role) {
+        this.id = id;
         this.email = email;
         this.password = password;
         this.nickName = nickName;
