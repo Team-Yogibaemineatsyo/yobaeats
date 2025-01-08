eed40053621a34f9230d439d00bc22d7eee0e2e2
legoChoi <chlx159@gmail.com>
2025-01-08 11:28:32 +0900

    feat: Auth 기본 서비스 레이어 세팅

--- src/main/java/com/sparta/yobaeats/domain/auth/controller/AuthController.java
+++ src/main/java/com/sparta/yobaeats/domain/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.sparta.yobaeats.domain.auth.controller;
 
 import com.sparta.yobaeats.domain.auth.dto.AuthLoginResponse;
+import com.sparta.yobaeats.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,19 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("/signup")
     public ResponseEntity<Void> signup () {
+        authService.signup();
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthLoginResponse> login() {
-
+        authService.login();
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout () {
+        authService.logout();
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/sparta/yobaeats/domain/auth/service/AuthService.java
@@ -0,0 +1,21 @@
+package com.sparta.yobaeats.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    public void signup() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
+}
