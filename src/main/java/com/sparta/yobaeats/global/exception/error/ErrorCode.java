422071251177bbfe17847ca025c4f5284884a945
SeuneKeun <eiziong1@gmail.com>
2025-01-08 16:23:44 +0900

    fix: 병합 충돌 처리

--- src/main/java/com/sparta/yobaeats/global/exception/error/ErrorCode.java
+++ src/main/java/com/sparta/yobaeats/global/exception/error/ErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    INVALID_USER_ROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 권한입니다."),
+
     // store
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "가게가 존재하지 않습니다."),
     STORE_ALREADY_DELETED(HttpStatus.CONFLICT, "이미 폐업 처리된 가게입니다.");
