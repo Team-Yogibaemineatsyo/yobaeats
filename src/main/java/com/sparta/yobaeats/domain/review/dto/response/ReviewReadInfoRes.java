2157e4e55089517132584c39fa02c5bb66c9e9dd
goo3oo <goo3oo0918@gmail.com>
2025-01-09 20:55:25 +0900

    refactor: ReviewReadInfoRes 변경 - 클래스명 변경 reviewRes -> ReviewReadInfoRes - @Builder(access = AccessLevel.PRIVATE) 추가로 new 생성 방지 - ReviewReadInfoRes.from() 메서드 변경: new 생성 -> builder()

---
+++ src/main/java/com/sparta/yobaeats/domain/review/dto/response/ReviewReadInfoRes.java
@@ -0,0 +1,21 @@
+package com.sparta.yobaeats.domain.review.dto.response;
+
+import com.sparta.yobaeats.domain.review.entity.Review;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ReviewReadInfoRes(
+    Long reviewId,
+    int star,
+    String contents
+) {
+
+    public static ReviewReadInfoRes from(Review review) {
+        return ReviewReadInfoRes.builder()
+            .reviewId(review.getId())
+            .star(review.getStar())
+            .contents(review.getContents())
+            .build();
+    }
+}
