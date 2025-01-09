de2ed2ebc050c23c647be9687b1fcf495b5e62ca
goo3oo <goo3oo0918@gmail.com>
2025-01-09 23:33:04 +0900

    refactor: UserController 변경 - 경로 변경: /api/users/me

--- src/main/java/com/sparta/yobaeats/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/yobaeats/domain/user/controller/UserController.java
@@ -1,59 +1,53 @@
 package com.sparta.yobaeats.domain.user.controller;
 
+import com.sparta.yobaeats.domain.auth.entity.UserDetailsCustom;
 import com.sparta.yobaeats.domain.user.dto.request.UserDeleteReq;
-import com.sparta.yobaeats.domain.user.dto.response.UserRes;
-import com.sparta.yobaeats.domain.user.dto.request.UserUpdateReq;
+import com.sparta.yobaeats.domain.user.dto.response.UserReadInfoRes;
+import com.sparta.yobaeats.domain.user.dto.request.UserUpdateInfoReq;
 import com.sparta.yobaeats.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users/me")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserRes> findUserById(
-        // @AuthenticationPrincipal UserDetails userDetails,
-        @PathVariable Long userId
+    @GetMapping
+    public ResponseEntity<UserReadInfoRes> findUserById(
+        @AuthenticationPrincipal UserDetailsCustom userDetails
     ) {
-        // userId? email?
-        // String email =userDetails.getId(); ( 일단 대충..나중에 시큐리티 보고 수정 )
-        UserRes userRes = userService.findById(userId);
+        Long userId = userDetails.getId();
+        UserReadInfoRes userRes = userService.findById(userId);
         return ResponseEntity.ok(userRes);
     }
 
-    @PatchMapping("/{userId}")
+    @PatchMapping
     public ResponseEntity<Void> updateUser(
-        @Valid @RequestBody UserUpdateReq req,
-        @PathVariable Long userId
-        // @AuthenticationPrincipal UserDetails userDetails
-
+        @RequestBody @Valid UserUpdateInfoReq req,
+        @AuthenticationPrincipal UserDetailsCustom userDetails
     ) {
-        // userId? email?
-        // String email = userDetails.getId(); ( 일단 대충..나중에 시큐리티 보고 수정 )
+        Long userId = userDetails.getId();
         userService.updateUser(userId, req);
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{userId}")
+    @DeleteMapping
     public ResponseEntity<Void> deleteUser(
-        @Valid @RequestBody UserDeleteReq req,
-        @PathVariable Long userId
-        // @AuthenticationPrincipal UserDetails userDetails
+        @RequestBody @Valid UserDeleteReq req,
+        @AuthenticationPrincipal UserDetailsCustom userDetails
     ) {
-        // userId? email?
-        // String email = userDetails.getId(); ( 일단 대충..나중에 시큐리티 보고 수정 )
+        Long userId = userDetails.getId();
         userService.deleteUser(req, userId);
         return ResponseEntity.noContent().build();
     }
