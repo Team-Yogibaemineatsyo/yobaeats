5b8585f408fe64462c249c5d268f120914a14b0b
goo3oo <goo3oo0918@gmail.com>
2025-01-09 19:43:55 +0900

    refactor: UserUpdatedReq 변경 - @NotNull 에너테이션 삭제

--- src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserUpdateReq.java
+++ src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserUpdateReq.java
@@ -6,18 +6,14 @@ import jakarta.validation.constraints.Size;
 
 public record UserUpdateReq(
 
-    @NotNull(message = "메일은 필수 입력 항목입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "잘못된 이메일 형식입니다.")
     String email,
 
-    @NotNull(message = "비밀번호는 필수 입력 항목입니다.")
     @Pattern(regexp = "^(?=(.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~!@#$-_+.,:?])).{8,}$", message = "잘못된 비밀번호 형식입니다.")
     String password,
 
-    @NotNull(message = "닉네임은 필수 입력 항목입니다.")
     @Size(min = 1, max = 10, message = "리뷰 내용은 1자 이상 10자 이하로 입력해주세요.")
     String nickName
 ) {
-
 }
 
