a30792bd3cfcd51a4626f8ae0da1800b0c220c67
goo3oo <goo3oo0918@gmail.com>
2025-01-09 23:22:35 +0900

    refactor: UserUpdateInfoReq, UserDeleteReq 변경 - UserUpdateInfoReq: 수정에 부적절한 email 필드 삭제 - UserUpdateInfoReq: 부적절한 password 정규식 변경 - UserDeleteReq: 불필요한 password 정규식 삭제

--- src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserDeleteReq.java
+++ src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserDeleteReq.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Pattern;
 
 public record UserDeleteReq(
     @NotNull(message = "비밀번호는 필수 입력 항목입니다.")
-    @Pattern(regexp = "^(?=(.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~!@#$-_+.,:?])).{8,}$", message = "잘못된 비밀번호 형식입니다.")
     String password
 ) {
 }
--- src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserUpdateInfoReq.java
+++ src/main/java/com/sparta/yobaeats/domain/user/dto/request/UserUpdateInfoReq.java
@@ -1,16 +1,15 @@
 package com.sparta.yobaeats.domain.user.dto.request;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateInfoReq(
-    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "잘못된 이메일 형식입니다.")
-    String email,
-
-    @Pattern(regexp = "^(?=(.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[~!@#$-_+.,:?])).{8,}$", message = "잘못된 비밀번호 형식입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+]).{8,}$", message = "잘못된 비밀번호 형식입니다.")
     String password,
 
-    @Size(min = 1, max = 10, message = "리뷰 내용은 1자 이상 10자 이하로 입력해주세요.")
+
+    @Size(min = 1, max = 10, message = "닉네임은 10자 내로 입력해주세요.")
     String nickName
 ) {
 }
