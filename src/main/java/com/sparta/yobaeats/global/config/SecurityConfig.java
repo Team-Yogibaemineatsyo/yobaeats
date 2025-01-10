c5472dd2ae629736c95881656a49b06c1ffca046
legoChoi <chlx159@gmail.com>
2025-01-10 10:31:04 +0900

    refactor: SecurityConfig 수정
    - Method chaining

--- src/main/java/com/sparta/yobaeats/global/config/SecurityConfig.java
+++ src/main/java/com/sparta/yobaeats/global/config/SecurityConfig.java
@@ -5,14 +5,17 @@ import com.sparta.yobaeats.global.jwt.JwtAuthenticationFilter;
 import com.sparta.yobaeats.global.jwt.JwtUtil;
 import com.sparta.yobaeats.global.security.SecurityAccessDeniedHandler;
 import com.sparta.yobaeats.global.security.SecurityAuthenticationEntryPoint;
+import com.sparta.yobaeats.global.security.SecurityAuthenticationFailureHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -24,36 +27,40 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
 
     @Bean
+    public SecurityAuthenticationFailureHandler securityAuthenticationFailureHandler() {
+        return new SecurityAuthenticationFailureHandler();
+    }
+
+    @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(auth -> auth.disable());
-        http.formLogin(auth -> auth.disable());
-        http.httpBasic(auth -> auth.disable());
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
 
-        // 세션 stateless
-        http.sessionManagement((session) ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                // 세션 stateless
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-        // 접근 권한
-        http.authorizeHttpRequests(auth -> auth
+                // 접근 권한
+                .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll() // 모든 권한 허용
                 .requestMatchers("/api/users/**").hasRole("USER")
-                .anyRequest().hasRole("USER") // 나머지 로그인한 사용자만 접근 가능
-        );
+                .anyRequest().hasRole("USER")) // 나머지 로그인한 사용자만 접근 가능
 
-        // filter
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                // filter
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
 
-        // exception handler
-        http.exceptionHandling(exception ->
-                exception.accessDeniedHandler(new SecurityAccessDeniedHandler(objectMapper)));
-        http.exceptionHandling(exception ->
-                exception.authenticationEntryPoint(new SecurityAuthenticationEntryPoint()));
-
-        return http.build();
+                // exception handler
+                .exceptionHandling(handler ->
+                        handler.authenticationEntryPoint(new SecurityAuthenticationEntryPoint()))
+                .exceptionHandling(handler ->
+                        handler.accessDeniedHandler(new SecurityAccessDeniedHandler(objectMapper)))
+                .build();
     }
 }
