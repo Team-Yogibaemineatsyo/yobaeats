55ca4e13882e2fec9a93cc8205b1c13c68d0d8ee
legoChoi <chlx159@gmail.com>
2025-01-10 11:30:59 +0900

    feat: Security Authentication 예외 처리
    - ErrorCode 관련 데이터 추가

--- src/main/java/com/sparta/yobaeats/global/security/handler/SecurityAuthenticationEntryPoint.java
+++ src/main/java/com/sparta/yobaeats/global/security/handler/SecurityAuthenticationEntryPoint.java
@@ -1,8 +1,14 @@
 package com.sparta.yobaeats.global.security.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sparta.yobaeats.global.exception.error.ErrorCode;
+import com.sparta.yobaeats.global.exception.error.ErrorResponse;
+import com.sparta.yobaeats.global.security.exception.SecurityAuthenticationFailedException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -12,10 +18,25 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         log.error("Authentication error: need to login");
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(ErrorCode.AUTHENTICATION_FAILED_EXCEPTION.getStatus().value());
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        new ErrorResponse(
+                                ErrorCode.AUTHENTICATION_FAILED_EXCEPTION.getStatus(),
+                                ErrorCode.AUTHENTICATION_FAILED_EXCEPTION.getMessage()
+                        )
+                )
+        );
+//        throw new SecurityAuthenticationFailedException(ErrorCode.AUTHENTICATION_FAILED_EXCEPTION);
     }
 }
