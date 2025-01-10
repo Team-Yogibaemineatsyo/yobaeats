2638c04d7087f45c7abc4701185ff33b011af01e
goo3oo <goo3oo0918@gmail.com>
2025-01-10 10:08:58 +0900

    refactor: User 기능 리펙토링 - 사용하지 않는 임포트 제거 - UserController: 단순한 변수명 디테일하게 변경 req -> userUpdateInfoReq,   메서드 명에서 ById 제거, findUser -> readUserInfo - UserDeleteReq, UserUpdateInfoReq: requestDto validation message 상수화 - UserValidationMessage: validation message 상수 클래스 추가

---
+++ src/main/java/com/sparta/yobaeats/domain/user/dto/UserValidationMessage.java
@@ -0,0 +1,8 @@
+package com.sparta.yobaeats.domain.user.dto;
+
+public class UserValidationMessage {
+
+    public static final String PASSWORD_BLANK_MESSAGE = "비밀번호를 입력해주세요.";
+    public static final String INVALID_PASSWORD_MESSAGE = "잘못된 비밀번호 형식입니다.";
+    public static final String NICKNAME_RANGE_MESSAGE = "닉네임은 10자 내로 입력해주세요.";
+}
