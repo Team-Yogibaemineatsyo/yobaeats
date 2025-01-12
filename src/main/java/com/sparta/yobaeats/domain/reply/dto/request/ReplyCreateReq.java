e80b571de3a406f619617400b5ebf969f9257b0b
goo3oo <goo3oo0918@gmail.com>
2025-01-13 03:15:12 +0900

    refactor: 댓글 Request DTO 밸리데이션 추가

--- src/main/java/com/sparta/yobaeats/domain/reply/dto/request/ReplyCreateReq.java
+++ src/main/java/com/sparta/yobaeats/domain/reply/dto/request/ReplyCreateReq.java
@@ -1,15 +1,19 @@
 package com.sparta.yobaeats.domain.reply.dto.request;
 
+import com.sparta.yobaeats.domain.reply.dto.ReplyValidationMessage;
 import com.sparta.yobaeats.domain.reply.entity.Reply;
+import com.sparta.yobaeats.domain.review.dto.ReviewValidationMessage;
 import com.sparta.yobaeats.domain.review.entity.Review;
 import com.sparta.yobaeats.domain.user.entity.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ReplyCreateReq(
-        @NotNull
+        @NotNull(message = ReplyValidationMessage.REVIEW_ID_BLANK_MESSAGE)
         Long reviewId,
-        @NotBlank
+        @NotBlank(message = ReplyValidationMessage.REPLY_BLANK_MESSAGE)
+        @Size(max = ReplyValidationMessage.REPLY_MAX, message = ReplyValidationMessage.REPLY_MAX_MESSAGE)
         String content
 ) {
 
--- src/main/java/com/sparta/yobaeats/domain/reply/dto/request/ReplyUpdateReq.java
+++ src/main/java/com/sparta/yobaeats/domain/reply/dto/request/ReplyUpdateReq.java
@@ -1,9 +1,12 @@
 package com.sparta.yobaeats.domain.reply.dto.request;
 
+import com.sparta.yobaeats.domain.reply.dto.ReplyValidationMessage;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record ReplyUpdateReq(
-        @NotBlank
+        @NotBlank(message = ReplyValidationMessage.REPLY_BLANK_MESSAGE)
+        @Size(max = ReplyValidationMessage.REPLY_MAX, message = ReplyValidationMessage.REPLY_MAX_MESSAGE)
         String content
 ) {
 }
