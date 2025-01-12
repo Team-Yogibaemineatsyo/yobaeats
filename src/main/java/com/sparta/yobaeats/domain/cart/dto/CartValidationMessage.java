b959fd0cbeb7203541398e8d9f17d3328d694d0e
SeuneKeun <eiziong1@gmail.com>
2025-01-12 18:36:16 +0900

    feat: 장바구니 validation 추가

---
+++ src/main/java/com/sparta/yobaeats/domain/cart/dto/CartValidationMessage.java
@@ -0,0 +1,10 @@
+package com.sparta.yobaeats.domain.cart.dto;
+
+public class CartValidationMessage {
+
+    public static final String CART_ITEM_MENU_ID_NOT_NULL_MESSAGE = "메뉴가 제대로 선택되지 않았습니다.";
+    public static final int CART_ITEM_QUANTITY_MIN = 1;
+    public static final String CART_ITEM_QUANTITY_MIN_MESSAGE = "수량이 제대로 선택되지 않았습니다.";
+    public static final String CART_ITEM_QUANTITY_NOT_NULL_MESSAGE = "수량이 선택되지 않았습니다.";
+    public static final String CART_ITEM_INCREASE_NOT_NULL_MESSAGE = "수량 증가 옵션이 제대로 선택되지 않았습니다.";
+}
--- src/main/java/com/sparta/yobaeats/domain/cart/dto/request/CartItemQuantityUpdateReq.java
+++ src/main/java/com/sparta/yobaeats/domain/cart/dto/request/CartItemQuantityUpdateReq.java
@@ -1,9 +1,10 @@
 package com.sparta.yobaeats.domain.cart.dto.request;
 
+import com.sparta.yobaeats.domain.cart.dto.CartValidationMessage;
 import jakarta.validation.constraints.NotNull;
 
 public record CartItemQuantityUpdateReq(
-        @NotNull
+        @NotNull(message = CartValidationMessage.CART_ITEM_INCREASE_NOT_NULL_MESSAGE)
         Boolean increase
 ) {
 }
